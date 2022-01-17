1c8dc2df0997f1baed08fa5bfda81c63e5958bdc
Elias Peteri <elias.peteri@hotmail.com>
2022-01-17 23:26:55 +0200

    Created hook to access API.

---
+++ client/hooks/useResource.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosResponse } from "axios"
+
+const useResource = (url: string) => {
+  const getAll = async () => {
+    const res: AxiosResponse = await axios.get(url)
+    return res.data
+  }
+
+  const getByID = async (id: number) => {
+    const res: AxiosResponse = await axios.get(`${url}/${id}`)
+    return res.data
+  }
+
+  const create = async (obj: Record<string, unknown>) => {
+    const res: AxiosResponse = await axios.post(url, obj)
+    return res.data
+  }
+
+  const update = async (id: number, obj: Record<string, unknown>) => {
+    const res: AxiosResponse = await axios.put(`${url}/${id}`, obj)
+    return res.data
+  }
+
+  const remove = async (id: number) => {
+    const res: AxiosResponse = await axios.delete(`${url}/${id}`)
+    return res.data
+  }
+
+  return { getAll, getByID, create, update, remove }
+}
+
+export default useResource
