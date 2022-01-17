bec14cee30507985bf240678b28aec548a862a61
Elias Peteri <elias.peteri@hotmail.com>
2022-01-17 23:04:09 +0200

    Titles work as links now.

--- client/components/Book.tsx
+++ client/components/Book.tsx
@@ -1,14 +1,17 @@
+import Link from "next/link"
 import { Container, Divider, Header } from "semantic-ui-react"
 
-export default function Book({ author, description, title }) {
+export default function Book({ author, description, id, title }) {
   return (
     <Container text fluid>
-      <Divider />
-      <Header as="h2">
-        {title} by {author}
-      </Header>
+      <Link href={`/books/${id}`}>
+        <a>
+          <Header as="h2" dividing>
+            {title} by {author}
+          </Header>
+        </a>
+      </Link>
       {description}
-      <Divider />
     </Container>
   )
 }
