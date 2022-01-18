f4d00ceaa0521e1c716943abe5f9c20012740ccc
Elias Peteri <elias.peteri@hotmail.com>
2022-01-18 12:29:02 +0200

    Fixed RecentBooks on homepage?

--- client/components/RecentBooks.tsx
+++ client/components/RecentBooks.tsx
@@ -1,21 +1,29 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Header, List } from "semantic-ui-react"
 import { IBook } from "../types"
-
+import Link from "next/link"
 /**
  * Displays books recently added by the user (eventually will show recent results from the database)
  * @returns JSX.Element
  */
 export default function RecentBooks(): JSX.Element {
-  const recentBooks: [Omit<IBook, "id">] =
-    JSON.parse(localStorage.getItem("recentBooks")) || []
+  const [recentBooks, setRecentBooks] = useState([])
+  useEffect((): void => {
+    setRecentBooks(JSON.parse(window.localStorage.getItem("recentBooks")) || [])
+  })
   return (
     <>
       <Header as="h2">Recent Books added by readers like you</Header>
-      <List>
+      <List size={"huge"}>
         {recentBooks.map(
           (book: IBook): JSX.Element => (
-            <List.Item>{book.title}</List.Item>
+            <List.Item key={book.id}>
+              <Link href={`/books/${book.id}`}>
+                <a>
+                  <i>{book.title}</i> by <b>{book.author}</b>
+                </a>
+              </Link>
+            </List.Item>
           )
         )}
       </List>
