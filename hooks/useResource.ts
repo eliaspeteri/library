e57dc67eb5776f0e036717fe22b67ffdf05ad342
Elias Peteri <elias.peteri@hotmail.com>
2022-01-28 15:22:20 +0200

    Typed API response.

--- hooks/useResource.ts
+++ hooks/useResource.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse } from "axios"
-
+import { IBook } from "../types"
 /**
  * A hook to help with talking to the API through axios.
  * @param url API url
@@ -7,27 +7,27 @@ import axios, { AxiosResponse } from "axios"
  */
 const useResource = (url: string) => {
   const getAll = async () => {
-    const res: AxiosResponse = await axios.get(url)
+    const res: AxiosResponse<IBook[]> = await axios.get(url)
     return res.data
   }
 
   const getByID = async (id: string) => {
-    const res: AxiosResponse = await axios.get(`${url}/${id}`)
+    const res: AxiosResponse<IBook> = await axios.get(`${url}/${id}`)
     return res.data
   }
 
   const create = async (obj: Record<string, unknown>) => {
-    const res: AxiosResponse = await axios.post(url, obj)
+    const res: AxiosResponse<IBook> = await axios.post(url, obj)
     return res.data
   }
 
   const update = async (id: string, obj: Record<string, unknown>) => {
-    const res: AxiosResponse = await axios.put(`${url}/${id}`, obj)
+    const res: AxiosResponse<IBook> = await axios.put(`${url}/${id}`, obj)
     return res.data
   }
 
   const remove = async (id: string) => {
-    const res: AxiosResponse = await axios.delete(`${url}/${id}`)
+    const res: AxiosResponse<IBook> = await axios.delete(`${url}/${id}`)
     return res.data
   }
 
