628e94cbc1f5bb289a84c8ebcc9eefe1a91ad2f0
Elias Peteri <elias.peteri@hotmail.com>
2022-01-31 17:55:51 +0200

    Removed unnecessary call to React namespace.

--- context/ToastContext.tsx
+++ context/ToastContext.tsx
@@ -3,7 +3,7 @@ import React, { Context, createContext, useContext, useState } from "react"
 
 /** Default values */
 // Toast message
-const ToastContext: Context<string> = React.createContext("")
+const ToastContext: Context<string> = createContext("")
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 const ToggleToastContext = createContext((_newToastMsg: string) => {})
 
@@ -20,7 +20,6 @@ const ToastProvider: React.FC<Props> = ({ children }: Props): JSX.Element => {
   const [msg, setMsg] = useState<string>("")
 
   const toggleToast = (newMsg: string): void => setMsg(newMsg)
-
   return (
     <ToastContext.Provider value={msg}>
       <ToggleToastContext.Provider value={toggleToast}>
